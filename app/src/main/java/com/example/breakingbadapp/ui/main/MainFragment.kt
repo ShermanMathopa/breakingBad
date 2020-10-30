114ade5f4164d12ce56168b92cd74efa775c7ecc
Sherman <sherman@glucode.com>
2020-10-31 00:14:22 +0200

    Add character_view fragment elements

--- app/src/main/java/com/example/breakingbadapp/ui/main/MainFragment.kt
+++ app/src/main/java/com/example/breakingbadapp/ui/main/MainFragment.kt
@@ -1,15 +1,13 @@
 package com.example.breakingbadapp.ui.main
 
-import androidx.lifecycle.ViewModelProviders
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.navigation.fragment.findNavController
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProviders
 import com.example.breakingbadapp.R
 import com.example.breakingbadapp.framework.viewmodels.MainViewModel
-import kotlinx.android.synthetic.main.main_fragment.*
 
 class MainFragment : Fragment() {
 
